2c02606495f99c9ee913fc3134dabb23b76f4d25
nawab-mac <bondkn@gmail.com>
2021-03-10 12:11:19 +0530

    commit with h2

--- src/main/java/com/example/jpa2/MainController.java
+++ src/main/java/com/example/jpa2/MainController.java
@@ -1,28 +1,40 @@
 package com.example.jpa2;
 
-import com.example.jpa2.dto.Club;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainController {
 
-    @Autowired
-    JPARepository jpaRepository;
+   @Autowired
+    StudentService studentService;
 
-    /*@GetMapping("getClubs")
-    public List<Club> getName(String name){
-        List<Club> clubs = jpaRepository.findByClub(name);
-        return clubs;
-    }*/
-
-    @GetMapping("getClubs")
-    public String getName(String name){
-        //List<Club> clubs = jpaRepository.findByClub(name);
-        return "nawab";
+    //creating a get mapping that retrieves all the students detail from the database
+    @GetMapping("/student")
+    private List<Student> getAllStudent()
+    {
+        return studentService.getAllStudent();
+    }
+    //creating a get mapping that retrieves the detail of a specific student
+    @GetMapping("/student/{id}")
+    private Student getStudent(@PathVariable("id") int id)
+    {
+        return studentService.getStudentById(id);
+    }
+    //creating a delete mapping that deletes a specific student
+    @DeleteMapping("/student/{id}")
+    private void deleteStudent(@PathVariable("id") int id)
+    {
+        studentService.delete(id);
+    }
+    //creating post mapping that post the student detail in the database
+    @PostMapping("/student")
+    private int saveStudent(@RequestBody Student student)
+    {
+        studentService.saveOrUpdate(student);
+        return student.getId();
     }
 }
